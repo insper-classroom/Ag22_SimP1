3d7ca7ca3d2e14405182991584e8f5e485d9705f
Fabio R de Miranda mirwox <5264186+mirwox@users.noreply.github.com>
2022-09-22 10:27:22 -0300

    envios Q3

--- src/model/quadronegro.py
+++ src/model/quadronegro.py
@@ -18,10 +18,12 @@ class Professor(object):
         self.turmas = [] 
 
 class Estudante(object):
+    estudantes = []
     def __init__(self, nome):    
         self.nome = nome
         self.turmas = [] 
         self.submissoes = []
+        Estudante.estudantes.append(self)
 
     def matricular(self, turma):
         self.turmas.append(turma)
@@ -91,31 +93,119 @@ class Submissao(object):
 class FachadaTarefa:
     def listar_tarefas():
         """Deve retornar uma lista com o nome de todas as tarefas"""
-        pass
+
+        # Notas: 
+        # as tarefas não tinham atributo nome, por isso usaremos o campo gabarito
+        # se você criou um atributo nome estará ok
+        nome_tarefas = [tarefa.gabarito for tarefa in Tarefa.tarefas]
+
+        return nome_tarefas
+        
 
     def listar_notas_estudante(nome_tarefa:str, nome_estudante:str):
         """Deve retornar uma lista com as notas que o estudante obteve em todas as suas submissões para uma dada tarefa"""
-        pass 
+        
+        # Vamos novamente considerar o gabarito da tarefa em vez de seu nome
+        # O desafio é que a única classe que guarda referências permanentemente são as tarefas
+        # Criamos a lista Estudante.estudantes para resolver isso
+        # Numa situação de prova em que é impossível resolver de outro jeito isso 
+        # seria permitido
+        submissoes = Tarefa.submissoes
+        estudantes = Estudante.estudantes
+
+        estud = None
+
+        # Encontra o estudante (supondo que não haja repetições)
+        for e in estudantes:
+            if e.nome == nome_estudante:
+                estud = e
+
+        notas = []
+
+        for s in estud.submissoes: 
+            if s.tarefa.gabarito == nome_tarefa: 
+                notas.append(s.nota)
+
+        return notas 
 
     def listar_disciplinas(nome_estudante:str): 
         """Deve retornar os nomes de todas as disciplinas que alguém cursa"""
-        pass 
+        # Vamos usar novamente a lista global de estudantes
 
+        estudantes = Estudante.estudantes
+
+        estud = None
+
+        # Encontra o estudante (supondo que não haja repetições)
+        for e in estudantes:
+            if e.nome == nome_estudante:
+                estud = e
+
+        retorno = []
+
+        for t in estud.turmas:
+            d = t.disciplina.nome 
+            retorno.append(d)
+        
+        return retorno 
+
+
+
+
+
+        
 if __name__ == "__main__":
     d = Disciplina("DevLife")
     tur = Turma(d, "DevLife 2022/1")
+
+    d2 = Disciplina("A Bug's Life")
+    tur2 = Turma(d2, "B Life 2022/2")
+
+    d3 = Disciplina("The Life of Pi")
+    tur3 = Turma(d3, "Pi Life 2022/2")
+
+    d4 = Disciplina("The Life of Brian")
+    tur4 = Turma(d4, "Brian Life 2022/2")
+
+
     estudante = Estudante("Diana Deana")
+    
     estudante.matricular(tur)
+    estudante.matricular(tur2)
+    estudante.matricular(tur3)
+    estudante.matricular(tur4)
+    
     tarefa = Tarefa(tur, "Pedro Álvares Cabral")
     tarefa2 = Tarefa(tur, "Cristóvão Colombo") # só para deixar o teste melhor
 
     tarefa.submeter("Pedro A", estudante, datetime.datetime(2022, 9, 16))
+    tarefa.submeter("Pedro Álvares", estudante)    
+    
     tarefa2.submeter("Francisco Pizarro", estudante, datetime.datetime(2022, 9, 16))
 
     # Testando a Q1
     resp = Tarefa.listar_submissoes_aluno(estudante)
     for r in resp:
         print(r)
+
+    # Testes das novas funções
+
+    tarefas = FachadaTarefa.listar_tarefas()
+    print("Todas as tarefas")
+    print(tarefas)
+
+    disc = FachadaTarefa.listar_disciplinas("Diana Deana")
+    print("Disciplinas da aluna")
+    print(disc)
+
+    notas_estudante = FachadaTarefa.listar_notas_estudante("Pedro Álvares Cabral", "Diana Deana")
+    print("Notas numa disciplina")
+    print(notas_estudante)
+
+
+
+
+
     
 
 
